f1aa0f14015e16c417fb3f29b4529468e04c3ce4
zelalem26 <zelalemfiseha00@gmail.com>
2023-02-16 14:13:27 -0800

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - a program that print the output with puts
+ *
+ * Return : 0 (Success)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
