d195f4cf551df6729922f82cb3f51a8e3783ec85
zelalem26 <zelalemfiseha00@gmail.com>
2023-02-16 14:55:15 -0800

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,10 +2,10 @@
 
 /**
  * main - print the output with printf
- *
  * Return: 0 (Success)
  */
-int main(void) 
+
+int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
