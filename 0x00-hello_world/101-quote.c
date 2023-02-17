9e3acbf6c2b9a4709a3969ca80c68c2066f06fe1
zelalem26 <zelalemfiseha00@gmail.com>
2023-02-16 16:02:57 -0800

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,11 +3,10 @@
 
 /**
  * main - a program that print a line to the standard error
- *
  * Return: 1 (Success)
  */
 int main(void)
 {
-	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
 	return (1);
 }
